b9d5dc886fd43165a848018b4e330a19e682bc96
Felipe Ribeiro <piporibeiro@gmail.com>
2022-01-03 20:47:55 -0300

    styles: adjust logout button

--- src/components/ButtonIcon/styles.ts
+++ src/components/ButtonIcon/styles.ts
@@ -8,8 +8,8 @@ type Props = {
 export const Container = styled.TouchableOpacity<Props>`
   justify-content: center;
   align-items: center;
-  border-radius: 5px;
+  border-radius: 8px;
   background-color:  ${({ color, theme }) => color === 'success' ? theme.COLORS.GREEN : theme.COLORS.RED};
-  width: ${({ size }) => size === 'small' ? 40 : 60}px;
-  height: ${({ size }) => size === 'small' ? 40 : 60}px;
+  width: ${({ size }) => size === 'small' ? 35 : 50}px;
+  height: ${({ size }) => size === 'small' ? 35 : 50}px;
 `;
